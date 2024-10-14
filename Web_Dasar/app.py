19e2328cd9c54b358470ec814d974d8ef1279120
Robby Saidi Prasetyo <157108492+Robbysaidiii@users.noreply.github.com>
2024-10-14 13:45:27 +0700

    penambahan baru

--- Web_Dasar/app.py
+++ Web_Dasar/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
-app.static_folder = 'static'
+
+@app.route('/assets/<path:path>')
+def send_assets(path):
+    return send_from_directory('assets', path)
 
 @app.route("/")
 def home():
@@ -11,10 +14,16 @@ def home():
 def profile():
     return render_template("profile-bootstrap.html")
 
-@app.route("/form")
+@app.route("/form", methods=["GET", "POST"])
 def form():
+    if request.method == "POST":
+        nama_lengkap = request.form["nama_lengkap"]
+        return render_template("form-bootstrap.html", message="Form submitted successfully!")
     return render_template("form-bootstrap.html")
 
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
